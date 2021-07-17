5adf00c89889f4958013e7e66dd69500ae2cfd4b
Manabu Niseki <manabu.niseki@gmail.com>
2021-07-17 11:16:17 +0900

    chore: update dependencies

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,5 @@
-export const dependencyRegexp = /^([a-zA-z0-9-_]+)\s*=+\s*.+["|']?\^*([0-9.]+)["|']?.+/;
+export const dependencyRegexp =
+  /^([a-zA-z0-9-_]+)\s*=+\s*.+["|']?\^*([0-9.]+)["|']?.+/;
 export const packageNameRegexp = /^([a-zA-z0-9-_]+)\s*=+\s*/;
 export const packageRequirementsRegexp = /["|']?\^*([0-9.]+)["|']?/;
 export const bracketsRegexp = /\[[a-zA-z0-9-_]+\]/;
