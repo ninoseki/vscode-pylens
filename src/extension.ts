a5fa05bf775e3244fd84abf4049d68f8e2b7d0ce
Manabu Niseki <manabu.niseki@gmail.com>
2022-08-27 16:44:59 +0900

    refactor: set filters as an array

--- src/extension.ts
+++ src/extension.ts
@@ -3,57 +3,31 @@ import * as vscode from "vscode";
 import { AbstractProvider } from "./providers/abstractProvider";
 
 export function activate(context: vscode.ExtensionContext): void {
-  const pyprojectToml: vscode.DocumentFilter = {
-    pattern: "**/pyproject.toml",
-    scheme: "file",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      pyprojectToml,
-      new AbstractProvider()
-    )
-  );
+  const filters: vscode.DocumentFilter[] = [
+    {
+      pattern: "**/pyproject.toml",
+      scheme: "file",
+    },
+    {
+      pattern: "**/requirements.txt",
+      scheme: "file",
+    },
+    {
+      pattern: "**/requirements-*.txt",
+      scheme: "file",
+    },
+    {
+      pattern: "**/requirements/*.txt",
+      scheme: "file",
+    },
+  ];
+  const provider = new AbstractProvider();
 
-  const requirements: vscode.DocumentFilter = {
-    pattern: "**/requirements.txt",
-    scheme: "file",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(requirements, new AbstractProvider())
-  );
-
-  const requirementsEtc: vscode.DocumentFilter = {
-    pattern: "**/requirements-*.txt",
-    scheme: "file",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      requirementsEtc,
-      new AbstractProvider()
-    )
-  );
-
-  const requirementsEtc2: vscode.DocumentFilter = {
-    pattern: "**/*-requirements.txt",
-    scheme: "file",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      requirementsEtc2,
-      new AbstractProvider()
-    )
-  );
-
-  const requirementsSubDirectory: vscode.DocumentFilter = {
-    pattern: "**/requirements/*.txt",
-    scheme: "file",
-  };
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider(
-      requirementsSubDirectory,
-      new AbstractProvider()
-    )
-  );
+  filters.forEach((filter) => {
+    context.subscriptions.push(
+      vscode.languages.registerHoverProvider(filter, provider)
+    );
+  });
 }
 
 export function deactivate(): void {
