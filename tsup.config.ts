c8df049f311926edfd48863e3c8882ac21add9b4
Manabu Niseki <manabu.niseki@gmail.com>
2023-04-22 14:07:31 +0900

    v0.2.3

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   shims: false,
   dts: false,
   external: ["vscode"],
-  noExternal: ["axios-cache-interceptor", "axios", "camelcase-keys", "toml"],
+  noExternal: ["axios-cache-interceptor", "axios", "semver", "@iarna/toml"],
 });
